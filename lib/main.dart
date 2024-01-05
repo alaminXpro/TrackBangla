e37faed9d29a2dae33861ed83f2de9f3f8f96ed7
alaminXpro <alaminxpro@gmail.com>
2024-01-05 23:40:18 +0600

    Commit from AlaminXpro

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,58 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:trackbangla/blocs/internet_bloc.dart';
 import 'package:trackbangla/core/utils/initial_bindings.dart';
 import 'package:trackbangla/firebase_options.dart';
 import 'package:trackbangla/router/app_routes.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:provider/provider.dart';
+import 'package:nested/nested.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:io';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('en'), Locale('es')],
+      path: 'assets/translations',
+      fallbackLocale: Locale('en'),
+      startLocale: Locale('en'),
+      useOnlyLangCode: true,
+      child: MyApp(),
+    )
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Track Bangla',
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      initialRoute: AppRoutes.onBoarding,
-      getPages: AppRoutes.pages,
-      initialBinding: InitialBindings(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<InternetBloc>(create: (context) => InternetBloc(),)
+      ],
+      child: GetMaterialApp(
+        supportedLocales: context.supportedLocales,
+        localizationsDelegates: context.localizationDelegates,
+        locale: context.locale,
+        debugShowCheckedModeBanner: false,
+        title: 'Track Bangla',
+        theme: ThemeData.light(),
+        darkTheme: ThemeData.dark(),
+        initialRoute: AppRoutes.onBoarding,
+        getPages: AppRoutes.pages,
+        initialBinding: InitialBindings(),
+      ),
     );
   }
 }
